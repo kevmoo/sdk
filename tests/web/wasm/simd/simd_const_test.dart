0a7bbc991a07a622f7f0d065ecb739f09da5b10c
Kevin Moore <kevmoo@google.com>
2026-02-19 11:13:51 -0800

    Fix test header
    Change-Id: I0482a3a482bac6a2cdefbf1cf07f7e39999727fd

--- tests/web/wasm/simd/simd_const_test.dart
+++ tests/web/wasm/simd/simd_const_test.dart
@@ -1,3 +1,9 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// dart2wasmOptions=--extra-compiler-option=--enable-experimental-wasm-interop
+
 // ignore: import_internal_library
 import 'dart:_wasm';
 
